58d0b781d6073807605d0d62f8ef2ff20630c8d7
alexeyvalko <valko.alexey@gmail.com>
2021-03-14 17:26:43 +0300

    feat: task is complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newStr = str;
+    let newArr = [];
+    bracketsConfig.forEach((item) => {
+        newArr.push(item.join(""));
+    });
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (newStr.includes(newArr[i])) {
+            newStr = newStr.replace(newArr[i], "");
+            i = -1;
+        }
+    }
+
+    return newStr ? false : true;
+};
